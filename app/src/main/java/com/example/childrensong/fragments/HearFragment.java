ec0aaa4a1d4c808070375bb387d247d89be3e155
licong229265529 <a229265529>
2019-08-12 14:47:37 +0800

    toolbar

--- app/src/main/java/com/example/childrensong/fragments/HearFragment.java
+++ app/src/main/java/com/example/childrensong/fragments/HearFragment.java
@@ -1,5 +1,10 @@
 package com.example.childrensong.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 import com.example.childrensong.R;
 import com.example.childrensong.base.SimpleFragment;
 
@@ -13,5 +18,18 @@ public class HearFragment extends SimpleFragment {
     @Override
     protected int createLayout() {
         return R.layout.hear_fragment_layout;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+        getActivity().getMenuInflater().inflate(R.menu.ting_search_black,menu);
     }
 }
